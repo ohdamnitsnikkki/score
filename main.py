a80d7166261b1e9a2ee137c8ed3d78ccd062a3d4
ohdamnitsnikkki <mammastjernberg@gmail.com>
2025-10-20 20:14:00 +0000

    Add code to ask for a number between 1-100

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+# Frågar anvädaren om ett tal mellan 0-100
+
+def get_score(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if 0 <= value <= 100:
+                return value
+            else:
+                print("Ange ett tal mellan 0-100")
+        except ValueError:
+            print("Du måste skriva i siffror ej bokstäver")
+
+# Ta in tre poäng från användaren
